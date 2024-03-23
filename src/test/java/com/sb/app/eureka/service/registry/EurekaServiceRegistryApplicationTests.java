0a7cc6f8dcf406db906d34e6c5d712b167328ecd
arjun-bhalekar <arjunbhalekar95@gmail.com>
2024-03-23 20:20:54 +0530

    eureka registry service

---
+++ src/test/java/com/sb/app/eureka/service/registry/EurekaServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sb.app.eureka.service.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
